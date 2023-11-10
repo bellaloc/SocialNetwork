e301054e2fb2fc7ca0a442356446dd848f7ae6ff
Christa Lococo <montecitocandleco@gmail.com>
2023-11-10 15:50:33 -0700

    models route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,88 +1,82 @@
-const { User, Thought } = require('../models');
+const db = require('../models');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-const usersController = {
-  // Get all users
-  getAllUsers(req, res) {
-    console.log(User); // Log the User object
-    User.find({})
-      .select('-__v')
-      .populate({
-        path: 'thoughts',
-        select: '-__v',
-      })
-      .populate({
-        path: 'friends',
-        select: '-__v',
-      })
-      .then((users) => res.json(users))
-      .catch((err) => res.status(500).json(err));
+const userController = {
+  registerUser: async (req, res) => {
+    try {
+      const { username, password } = req.body;
+
+      if (!username || !password) {
+        return res.status(400).json({ error: 'Username and password fields are required' });
+      }
+
+      const existingUser = await db.User.findOne({ where: { username } });
+
+      if (existingUser) {
+        return res.status(400).json({ error: 'Username is already taken' });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+      const user = await db.User.create({ username, password: hashedPassword });
+
+      req.session.userId = user.id;
+
+      res.status(201).json({ user });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to register user' });
+    }
   },
 
-  // Get a single user by their _id and populate thought and friend data
-  getUserById(req, res) {
-    const { userId } = req.params;
-    User.findOne({ _id: userId })
-      .select('-__v')
-      .populate({
-        path: 'thoughts',
-        select: '-__v',
-      })
-      .populate({
-        path: 'friends',
-        select: '-__v',
-      })
-      .then((user) => {
-        if (!user) {
-          return res.status(404).json({ message: 'User not found' });
+  loginUser: async (req, res) => {
+    try {
+      const { username, password } = req.body;
+
+      if (!username || !password) {
+        return res.status(400).json({ error: 'Username and password fields are required' });
+      }
+
+      const user = await db.User.findOne({ where: { username } });
+
+      if (!user) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatch) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+
+      req.session.userId = user.id;
+
+      res.json({ user });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to login' });
+    }
+  },
+
+  logoutUser: (req, res) => {
+    try {
+      if (!req.session.userId) {
+        return res.status(401).json({ error: 'You must be logged in to logout' });
+      }
+
+      req.session.destroy((err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: 'Failed to logout' });
         }
-        res.json(user);
-      })
-      .catch((err) => res.status(500).json(err));
-  },
-
-  // Create a new user
-  createUser(req, res) {
-    const { username, email } = req.body;
-    User.create({ username, email })
-      .then((user) => res.json(user))
-      .catch((err) => res.status(400).json(err));
-  },
-
-  // Update a user by their _id
-  updateUser(req, res) {
-    const { userId } = req.params;
-    const { username, email } = req.body;
-
-    User.findOneAndUpdate({ _id: userId }, { username, email }, { new: true })
-      .then((user) => {
-        if (!user) {
-          return res.status(404).json({ message: 'User not found' });
-        }
-        res.json(user);
-      })
-      .catch((err) => res.status(400).json(err));
-  },
-
-  // Delete a user by their _id
-  deleteUser(req, res) {
-    const { userId } = req.params;
-
-    User.findOneAndRemove({ _id: userId })
-      .then((user) => {
-        if (!user) {
-          return res.status(404).json({ message: 'User not found' });
-        }
-        // Remove the user from the friends list of all other users
-        User.updateMany({ friends: userId }, { $pull: { friends: userId } })
-          .then(() => {
-            // Remove all thoughts associated with the user (Bonus Task)
-            return Thought.deleteMany({ _id: { $in: user.thoughts } });
-          })
-          .then(() => res.json(user))
-          .catch((err) => res.status(500).json(err));
-      })
-      .catch((err) => res.status(500).json(err));
+        res.status(204).end();
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to logout' });
+    }
   },
 };
 
-module.exports = usersController;
+module.exports = userController;
