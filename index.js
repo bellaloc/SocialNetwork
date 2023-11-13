2f391b610a2fff638b641f48770c733e2776ccb1
Christa Lococo <montecitocandleco@gmail.com>
2023-11-13 10:57:02 -0700

    fixed insomia api routes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const db = require('./config/connection');
-const routes = require('./routes/api');
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api/users', routes);
+app.use( routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
